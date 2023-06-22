7e430540c1ad660527bcdd6ef07cf26192efeeb2
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-06-22 11:57:30 +0700

    Cập nhật dự án

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Quizz(models.Model):
+    quizz_id = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.quizz_id
--- shop/settings.py
+++ shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+'shop/static',
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
