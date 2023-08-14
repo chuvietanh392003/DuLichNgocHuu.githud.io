b932363ce25d20414cd11e284e337f03d7c70d81
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-08-14 22:43:49 +0700

    deploy

--- shop/settings.py
+++ shop/settings.py
@@ -84,9 +84,14 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
