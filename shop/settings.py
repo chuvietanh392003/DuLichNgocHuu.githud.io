734a21a3ccb33653e5001b8bedafa5c85238df13
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-08-14 23:19:52 +0700

    change settings Database, allowed host for deploying

--- shop/settings.py
+++ shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
@@ -84,14 +84,16 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 #     }
 # }
 
-if not DEBUG:
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }}
+# if not DEBUG:
+#     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+DATABASES['default'] = dj_database_url.parse("postgres://postgres_sql_user:sQICZXKQnnW8CWsk3uZPF2aoYAD5nJ2X@dpg-cjcvne7db61s73adpa2g-a.singapore-postgres.render.com/postgres_sql")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
