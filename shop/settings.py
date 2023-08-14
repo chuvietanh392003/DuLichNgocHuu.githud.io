b4264a8787e48238b8f6134dab501df45ae9ec85
chuvietanh392003 <130541979+chuvietanh392003@users.noreply.github.com>
2023-08-14 23:56:05 +0700

    python manage.py collectstatic & adding missed whitenoise staticfiles_storage

--- shop/settings.py
+++ shop/settings.py
@@ -133,9 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
 'shop/static',
 ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
